06a5ab6b81c43b13e19607eef9ba0482cf765a96
qiaoqiangv <qiaoqiangv@126.com>
2017-03-04 01:07:50 +0800

    backup on Ubuntu 16.04.2  at 2017年 03月 04日 星期六 01:07:50 CST

--- ShellScripts/add-TODO-list.sh
+++ ShellScripts/add-TODO-list.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # this script to echo some word to TODO file
-	echo -en '\n'  >> ~/TODO
-	echo -e "$(date) ~~~ CONTENT : $1" >> ~/TODO
+	# test
+	#echo -e "\n$(date) ~~~ CONTENT : $1" > ./test
+	#echo -en '\n'  >> ./test
+
+	echo -e "\n$(date) - CONTENT : $1" >> ~/TODO
+
--- ShellScripts/backup.sh
+++ ShellScripts/backup.sh
@@ -8,25 +8,13 @@
 
 # use system command to checkout what different between the remote repository and local repository
 	DIFF_HOMEDIR_AND_GITIGNORE=~/diff-the-Home-dir-and-.gitignore-file.txt
- 
+
 	echo "*** this is Home directory's contant ***" > ${DIFF_HOMEDIR_AND_GITIGNORE}
-	echo " " >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-
-	ls -a ~/ >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-
-	echo " " >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-	echo " " >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-
+	echo -e "\n$(ls -a ~/)\n\n" >> ${DIFF_HOMEDIR_AND_GITIGNORE}
 	echo "*** this is .gitignore regulate the file ignore rule ***" >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-	echo " " >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-
-	cat ~/.gitignore >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-
-	echo " " >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-	echo " " >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-
+	echo -e "\n$(cat ~/.gitignore)\n\n" >> ${DIFF_HOMEDIR_AND_GITIGNORE}
 	echo "[end]" >> ${DIFF_HOMEDIR_AND_GITIGNORE}
-
+ 
 # use 'git' command backup Home folder to remote repository of the github
 	# this two method to get machine version have equal result
 	# MACHINE_VERSION=$(cat /etc/issue | awk '{print $1,$2}' | tr -d '\n')
@@ -41,9 +29,11 @@
 	git commit -m "backup on ${MACHINE_VERSION} at ${DATE}"
 	git push
 	
+	echo -e "\n\n========================================================"
+	echo -e "||  backup home folder to github remote repository !  ||"
+	echo -e "========================================================\n\n"
 	sleep 10
 
 	cd -
-
 
 # [end]
