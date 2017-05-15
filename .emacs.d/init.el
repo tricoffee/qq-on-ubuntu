26aaa78fc550419fee4a98d301a7c959eec4f2b6
qiaoqiangv <qiaoqiangv@126.com>
2017-05-15 14:38:21 +0800

    commit before pull at 一  5月 15 14:38:17 CST 2017

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,3 +33,26 @@
   (spacemacs/setup-startup-hook)
   (require 'server)
   (unless (server-running-p) (server-start)))
+
+
+;; my personal setup
+;; (add-to-list 'load-path (concat plugin-path "bingapiel"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/bingapiel"))
+;; Your bing api client_id.
+(defvar bingapi-clientid "667f3adb-e22d-4dca-b476-ca536c0a6d8a")
+;; Your bing api client_secret.
+(defvar bingapi-clientsecret "xBTJ5Ee5RSFf++uVjSVKVFcsoswQlDyb8kPp5wSyrV8=")
+;; list all language codes may be used
+(defvar bingtranslate-language-list '("en" "zh-CHS" "ja"))
+;; Turn auto detect language code on
+(defvar bingtranslate-auto-detect t)
+
+(require 'bing-translate)
+;; key bounding
+(global-set-key [M-f3] 'bingtranslate-region-or-input)
+;; add a pair of language
+;; Parameters: "pair name" "from language" "to language"
+(bingtranslate-add-pair "1" "zh-CHS" "ja")
+
+;;(add-to-list 'load-path "~/.emacs.d/bingapiel/")
+;;(require 'bingapiel)
